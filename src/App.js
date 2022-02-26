e40af38a0c350262df9e99710a5b728dbdddee4b
Andres Gutierrez Ramirez <andres.gutierrez@pivotree.com>
2022-02-26 18:36:14 +0100

    Redoing Everything

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg'
+import './App.css'
+import { Map } from './components/Map'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+function App() {
+  return <Map />
+}
+
+export default App
