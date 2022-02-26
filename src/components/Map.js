c398a9f92e60259ca29bea7c8e987a2bf6e9418e
Andres Gutierrez Ramirez <andres.gutierrez@pivotree.com>
2022-02-26 19:00:48 +0100

    Update map style

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,74 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactMapGl, { Marker } from 'react-map-gl'
-import allStations from '../data/pollution.json'
+import allParkingPlaces from '../data/allParkingPlaces.json'
 
 const MAPBOX_TOKEN =
   'pk.eyJ1IjoiYWd1dGllcnJlenIiLCJhIjoiY2wwM3l2c3QzMDhvNTNqb2d2bWc2eWVrdCJ9.CrtbBZvdz8bEzFtDv4y2zw'
 
-console.log(allStations)
-
 export const Map = () => {
-  const [hour, sethHour] = useState('11:00:00')
-
-  const filteredStation = allStations.filter((station) => station.time === hour)
-
-  console.log(filteredStation)
-
-  function perc2color(perc) {
-    console.log({ perc })
-    let r,
-      g,
-      b = 0
-    if (perc < 50) {
-      r = 255
-      g = Math.round(5.1 * perc)
-    } else {
-      g = 255
-      r = Math.round(510 - 5.1 * perc)
-    }
-    let h = r * 0x10000 + g * 0x100 + b * 0x1
-
-    console.log('#' + ('000000' + h.toString(16)).slice(-6))
-
-    return '#' + ('000000' + h.toString(16)).slice(-6)
-  }
-
   return (
     <div>
       <ReactMapGl
         initialViewState={{
-          latitude: 40.416775,
-          longitude: -3.70379,
+          latitude: 52.205276,
+          longitude: 0.119167,
           zoom: 14,
         }}
-        scrollZoom={false}
         style={{ width: '100vw', height: '100vh' }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/agutierrezr/cl045dpgn002c14qxhfr0z73u"
         mapboxAccessToken={MAPBOX_TOKEN}
       >
-        <Marker
-          key={allStations[0].id}
-          longitude={allStations[0].lon}
-          latitude={allStations[0].lat}
-          color="red"
-        >
-          <div className="circle"></div>
-        </Marker>
-        {filteredStation.map((station) => (
-          <Marker
-            key={station.id}
-            longitude={station.lon}
-            latitude={station.lat}
-            color="red"
-          >
-            <div
-              className="circle"
-              style={{
-                backgroundColor: perc2color(station.pollution_rate * 100),
-              }}
-            ></div>
-          </Marker>
-        ))}
+        {allParkingPlaces.length !== 0 &&
+          allParkingPlaces.map((station) => (
+            <Marker
+              key={station.id}
+              longitude={station.lon}
+              latitude={station.lat}
+              color="red"
+            />
+          ))}
       </ReactMapGl>
     </div>
   )
